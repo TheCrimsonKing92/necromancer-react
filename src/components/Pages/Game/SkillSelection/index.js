87184ef7c9abd114bf1e5da0495311ef79205298
Miles Grimes <12803415+TheCrimsonKing92@users.noreply.github.com>
2022-11-17 07:57:37 -0600

    Move temporary skill defs out of component body

--- src/components/Pages/Game/SkillSelection/index.js
+++ src/components/Pages/Game/SkillSelection/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import SkillButton from '../../../SkillButton';
 
+const banish = {
+    name: 'Banish',
+    description: 'Banishes lesser undead from this plane. Has no effect on the living, constructs, or greater undead.',
+    tree: 'Combat Skills',
+    cost: '1 skill point'
+};
+
+const boneShield = {
+    name: 'Bone Shield',
+    description: 'Provides a shield with limited health, effective against physical damage.',
+    tree: 'Necromancer Combat Skills',
+    cost: '1 skill point'
+};
+
 const SkillSelection = props => {
     const { className, firstTimeSkills, setFooter } = props;
-
-    const banish = {
-        name: 'Banish',
-        description: 'Banishes lesser undead from this plane. Has no effect on the living, constructs, or greater undead.',
-        tree: 'Combat Skills',
-        cost: '1 skill point'
-    };
-
-    const boneShield = {
-        name: 'Bone Shield',
-        description: 'Provides a shield with limited health, effective against physical damage.',
-        tree: 'Necromancer Combat Skills',
-        cost: '1 skill point'
-    };
 
     return (
         <>
