8605dc325d39ba298bc3ae532b6883a70aa1e59a
Miles Grimes <12803415+TheCrimsonKing92@users.noreply.github.com>
2022-11-17 19:03:54 -0600

    Hide info title when no content is set

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 const Footer = ({ content }) => {
+    const hasContent = content !== null;
+
+    let infoStyles = {
+        fontSize: "1.25em"
+    };
+
+    if (!hasContent) {
+        infoStyles = {
+            ...infoStyles,
+            visibility: "hidden"
+        };
+    };
+
     return (
         <footer id="footer">
-            <p style={{fontSize: "1.25em"}}>
+            <p style={infoStyles}>
                 <strong>Info</strong>
             </p>
-            {content}
+            { hasContent && content}
         </footer>
     )
 };
