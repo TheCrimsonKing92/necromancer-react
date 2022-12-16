5ffad5464d6daa415b4aaa19916b62e212bba14b
Miles Grimes <12803415+TheCrimsonKing92@users.noreply.github.com>
2022-12-16 06:37:59 -0600

    Add WORLD_MAP scene

--- src/constants/Scenes/index.js
+++ src/constants/Scenes/index.js
@@ -1,9 +1,11 @@
 export const CHOOSE_CLASS = "CHOOSE_CLASS";
 export const CHOOSE_SKILL = "CHOOSE_SKILL";
+export const WORLD_MAP = "WORLD_MAP";
 
 const SCENES = {
     [CHOOSE_CLASS]: CHOOSE_CLASS,
-    [CHOOSE_SKILL]: CHOOSE_SKILL
+    [CHOOSE_SKILL]: CHOOSE_SKILL,
+    [WORLD_MAP]: WORLD_MAP
 };
 
 export default SCENES;
