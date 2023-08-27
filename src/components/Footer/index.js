1fc989ccfac06b6cb9d1bc763a622a1f176eaae3
Miles Grimes <12803415+TheCrimsonKing92@users.noreply.github.com>
2023-08-26 20:12:50 -0500

    Move visibility conditional style to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,19 +4,21 @@ import Container from 'react-bootstrap/Container';
 const Footer = ({ content }) => {
     const hasContent = content !== null;
 
+    let footerStyles = {};
+
     let infoStyles = {
         fontSize: "1.25em"
     };
 
     if (!hasContent) {
-        infoStyles = {
-            ...infoStyles,
+        footerStyles = {
+            ...footerStyles,
             visibility: "hidden"
         };
     };
 
     return (
-        <footer id="footer">
+        <footer id="footer" style={footerStyles}>
             <Container>
                 <p style={infoStyles}>
                     <strong>Info</strong>
