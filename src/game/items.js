19e2b25a0f3865dd927e750b910bcef875c1ca38
Miles Grimes <12803415+TheCrimsonKing92@users.noreply.github.com>
2025-03-18 22:44:59 -0500

    Begin item structure re-org

--- src/game/items.js
+++ src/game/items.js
@@ -1,26 +1,54 @@
-const Item = {
-    init({ name, description, effect, targetType }) {
-        this.name = name;
-        this.description = description;
-        this.effect = effect;
-        this.targetType = targetType;
-
-        return this;
-    },
-
+const BaseItem = {
     create(properties) {
         return Object.create(this).init(properties);
     },
 
-    use(user, targets) {
-        if (!this.effect) {
-            console.warn(`Item ${this.name} has no effect defined.`);
-            return {};
-        }
+    init({ name = "Unnamed Item", description = "", weight = 0 }) {
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
 
-        const updates = Object.fromEntries(targets.map(target => [ target.id, this.effect(user, target) ]));
-        return updates;
+        return this;
     }
 };
 
-export { Item };
+const EquippableItem = Object.create(BaseItem);
+
+EquippableItem.init = function(props) {
+    BaseItem.init.call(this, props);
+
+    this.skillBonuses = props.skillBonuses;
+    this.statBonuses = props.statBonuses;
+};
+
+const UsableItem = Object.create(BaseItem);
+
+UsableItem.init = function(props) {
+    BaseItem.init.call(this, props);
+
+    this.onUse = props.onUse;
+    if (props.uses) {
+        this.uses = props.uses;
+    }
+
+    return this;
+};
+
+UsableItem.use = function(user, targets) {
+    if (!this.onUse) {
+        console.warn(`Item '${this.name}' has no on-use effect defined`);
+        return;
+    }
+
+    this.onUse(user, targets);
+
+    if (this.uses) {
+        this.uses -= 1;
+    }
+
+    if (this.uses && this.uses < 1) {
+        // TODO: Destroy item/remove from inventory
+    }
+};
+
+export { BaseItem, EquippableItem, UsableItem };
